58a594333bb5ff20951e7172338b34d2c2ee7e43
Domenico Manasse <34721259+d3vd00m@users.noreply.github.com>
2021-03-17 00:09:27 +0100

    Update MakingtheZombieFactory.sol

--- MakingtheZombieFactory.sol
+++ MakingtheZombieFactory.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-//Create a contract called ZombieFactory
-
 contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
@@ -14,8 +14,19 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie (string memory _name, uint _dna) public {
-        // start here
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
     }
 
 }
