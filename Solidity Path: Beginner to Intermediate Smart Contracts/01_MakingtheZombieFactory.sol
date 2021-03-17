7b42cf3a0a86aa992998114d2caedfed281925c3
Domenico Manasse <34721259+d3vd00m@users.noreply.github.com>
2021-03-17 16:39:35 +0100

    Update 01_MakingtheZombieFactory.sol

--- Solidity Path: Beginner to Intermediate Smart Contracts/01_MakingtheZombieFactory.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/01_MakingtheZombieFactory.sol
@@ -8,39 +8,39 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
-    // Define an event to show when a new zombie is created
+    // Define an event to show when a new zombie is created.
 
     event NewZombie(uint zombieId, string name, uint dna);
 
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     
-    // Define a struct variable with two zombie's properties
+    // Define a struct variable with two zombie's properties.
     
     struct Zombie {
         string name;
         uint dna;
     }
     
-    // Define a public array of struct to store the army of zombies
+    // Define a public array of struct to store the army of zombies.
  
     Zombie[] public zombies;
     
-    // Define a private function to create the zombie
+    // Define a private function to create the zombie.
 
     function _createZombie(string memory _name, uint _dna) private {
     
     // Create the new zombie with the function arguments (Zombie(_name, _dna)) and add it to the zombies public array (zombie.push).
-    // Define the variable id that define the array index of the zombie we just added (the first array index is 0)
+    // Define the variable id that define the array index of the zombie we just added (the first array index is 0).
     
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         
-    // Call the event for the creation of the new zombie
+    // Call the event for the creation of the new zombie.
     
         emit NewZombie(id, _name, _dna);
     }
     
-    // Define a private function that generates a random DNA number from a string
+    // Define a private function that generates a random DNA number from a string.
     
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
