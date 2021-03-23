3f3121de7e5517f06558fda43655175a89893f9e
Domenico Manasse <34721259+d3vd00m@users.noreply.github.com>
2021-03-23 15:43:09 +0100

    Update zombiefeeding.sol

--- Solidity Path: Beginner to Intermediate Smart Contracts/Lesson 02 - Zombies Attack Their Victims/zombiefeeding.sol
+++ Solidity Path: Beginner to Intermediate Smart Contracts/Lesson 02 - Zombies Attack Their Victims/zombiefeeding.sol
@@ -8,9 +8,18 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-
+  
+  // It's time to give our zombies the ability to feed and multiply!
+  
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  
+    // We don't want to let someone else feed our zombie! So first, let's make sure we own this zombie.
+    
     require(msg.sender == zombieToOwner[_zombieId]);
+    
+    // Now the function declare a local Zombie named myZombie (which will be a storage pointer). 
+    // Set this variable to be equal to index _zombieId in our zombies array.
+    
     Zombie storage myZombie = zombies[_zombieId];
 
 }
